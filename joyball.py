acfd35734ddb8010db1e4235a8221aabe5898478
xiangtao <1427029356@qq.com>
2018-08-15 18:28:15 +0800

    集成cv2

--- joyball.py
+++ joyball.py
@@ -1,12 +1,16 @@
 from appium import webdriver
 import time
+import cv2
+
+img_path = "d:/1.png";
+img_cut_path = "d:/1_cut.png";
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['automationName'] = "uiautomator2" #使用uiautomator2定位，必须，否则定位不到
 desired_caps['deviceName'] = 'e7c976f2'
 desired_caps['unicodeKeyboard'] = True
-desired_caps["resetKeyboard"] = True
+desired_caps["resetKeyboard"] = False
 desired_caps["newCommandTimeout"] = 30
 desired_caps['fullReset'] = 'false'
 desired_caps['appPackage'] = 'com.tencent.mm'
@@ -22,10 +26,19 @@ time.sleep(1)
 driver.swipe(100, 1200, 100, 900)
 driver.find_element_by_xpath('//*[@text="小程序"]').click()
 time.sleep(1)
-
 driver.find_element_by_xpath('//*[@text="欢乐球球"]').click()
-time.sleep(5)
-time.sleep(2)
+print("准备关闭广告.")
+time.sleep(8)
 driver.tap([[916, 554]])
+print("关闭成功.")
+print("准备开始游戏.")
+time.sleep(5)
+driver.tap([[527, 1531]])
+print("开始游戏.")
+time.sleep(1)
+driver.save_screenshot(img_path)
+img = cv2.imread(img_path)
+edges = cv2.Canny(img, 50, 100)
+cv2.imwrite(img_cut_path, edges)
 time.sleep(5)
 driver.quit()
